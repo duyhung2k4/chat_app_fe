d1e55f57d426d36c8318fe258a6a3ed775b2f783
ndhung <nguyenduyhung04092004@gmail.com>
2024-07-28 02:42:44 +0700

    .

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,20 +1,28 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Box, Button, TextInput } from "@mantine/core";
 import { ProtectedLayoutContext, TypeProtectedLayoutContext } from "@/layout/protected";
+import { useAppSelector } from "@/redux/hook";
 
 const Home: React.FC = () => {
     const [text, setText] = useState<string>("");
+    const id = useAppSelector(state => state.authSlice.profile?.id);
 
     const { ws } = useContext<TypeProtectedLayoutContext>(ProtectedLayoutContext);
 
     const send = () => {
-        const data = JSON.stringify({ mess: text });
+        const data = JSON.stringify({
+            from_id: id,
+            to_id: 2,
+            box_chat_id: 1,
+            group_chat_id: null,
+            data: text
+        });
         ws?.send(data);
         setText("");
     }
 
     useEffect(() => {
-        if(!ws) return;
+        if (!ws) return;
         ws.onmessage = (e) => {
             const data = JSON.parse(e.data);
             console.log(data);
